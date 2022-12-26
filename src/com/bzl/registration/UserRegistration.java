e296125ee58e3486ad95d3d5e013df6694f152cc
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-26 12:50:17 +0530

    Adding uc1

--- src/com/bzl/registration/UserRegistration.java
+++ src/com/bzl/registration/UserRegistration.java
@@ -1,6 +1,29 @@
 package com.bzl.registration;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    String firstName;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        UserRegistration users = new UserRegistration();
+        users.addUser();
+
+    }
+    public void addUser(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Name : ");
+        String fName = sc.nextLine();
+        Pattern p1 = Pattern.compile("^[A-Z]+[a-z]{2,}");
+        Matcher mat = p1.matcher(fName);
+        boolean b = mat.matches();
+        if(b){
+            this.firstName = fName;
+        }else{
+            System.out.println("Invalid First name.");
+        }
+
     }
 }
